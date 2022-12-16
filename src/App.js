3e05dca36812425d9b94f82ca0c811bf72d4f21a
Utku Cengiz <cekicengiz01@gmail.com>
2022-12-16 09:31:48 +0300

    tailwind

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import styles from "./App.module.css";
 
 import { Title } from "./Components";
 
-import Bootstrap from "./Bootstrap";
+// import Bootstrap from "./Bootstrap";
+
+import "./tailwind.css";
+
+import Tailwind from "./Tailwind";
+
 function App() {
   return (
     <div className={styles.App}>
       <Title>Deneme</Title>
       <Title theme="dark">Deneme</Title>
       <Test />
-      <Bootstrap />
+      {/* <Bootstrap /> */}
+      <Tailwind />
     </div>
   );
 }
---
+++ src/Tailwind.js
@@ -0,0 +1,10 @@
+
+function Tailwind() {
+  return (
+    <div className="bg-blue-500">
+        <h3 className="text-xl font-semibold underline hover:bg-pink-600">Tailwind Başlık</h3>
+    </div>
+  )
+}
+
+export default Tailwind
