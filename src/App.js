5f670ad381f3093ce231a76ec436af46f82ddcf1
Utku Cengiz <cekicengiz01@gmail.com>
2022-12-16 09:17:52 +0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Test from "./Test";
 import styles from "./App.module.css";
 
 import { Title } from "./Components";
+
+import Bootstrap from "./Bootstrap";
 function App() {
   return (
     <div className={styles.App}>
       <Title>Deneme</Title>
       <Title theme="dark">Deneme</Title>
       <Test />
+      <Bootstrap />
     </div>
   );
 }
