d6766ee02982e378dd48606f4a0e8573f2406784
Utku Cengiz <cekicengiz01@gmail.com>
2022-12-18 02:35:36 +0300

    useReducer()

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import { forwardRef, useRef } from "react";
+import React from "react";
 
-const Input = forwardRef((props, ref) => {
-  return <input ref={ref} type="text" {...props} />;
-});
+import { useReducer } from "react";
+
+import todoreducer from "./reducers/todoReducer";
 
 function App() {
-  const inputRef = useRef();
-  const focusInput = () => {
-    inputRef.current.focus();
+  const [state, dispatch] = useReducer(todoreducer, {
+    todos: [],
+    todo: "",
+  });
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_TODO",
+      todo: state.todo,
+    });
   };
+
+  const onChange = (e) => {
+    dispatch({
+      type: "SET_TODO",
+      value: e.target.value,
+    });
+  };
+
   return (
     <>
-      <h1>useref() - forwardRef()</h1>
-      <Input title="test" ref={inputRef} />
-      <button onClick={focusInput}>Focusla</button>
+      <h1>Todo App</h1>
+      <form onSubmit={submitHandle}>
+        <input type="text" value={state.todo} onChange={onChange} />
+        <button disabled={!state.todo} type="submit">
+          Ekle
+        </button>
+      </form>
+      <ul>
+        {state.todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </>
   );
 }
