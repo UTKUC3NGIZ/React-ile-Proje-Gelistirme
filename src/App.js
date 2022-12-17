63e39d187ce8414893a0d713f48f2b8245491fdd
Utku Cengiz <cekicengiz01@gmail.com>
2022-12-17 04:15:15 +0300

    Component - Props - useState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import Test from "./Test";
-
-import styles from "./App.module.css";
-
-import { Title } from "./Components";
-
-// import Bootstrap from "./Bootstrap";
-
-// import "./tailwind.css";
-
-// import Tailwind from "./Tailwind";
-
-import "./style.scss";
+import { useEffect, useState } from "react";
+import Button from "./Components/Button";
+import Tab from "./Components/Tab";
+import "./tailwind.css";
 
 function App() {
+  const [activeTab, setActiveTab] = useState(1);
+
   return (
-    <div className={styles.App}>
-      <Title>Deneme</Title>
-      <Title theme="dark">Deneme</Title>
-      <Test />
-      {/* <Bootstrap /> */}
-      {/* <Tailwind /> */}
-      <p className="env">
-        <span>test</span>
-      </p>
-    </div>
+    <>
+      <div style={{ padding: 20 }}>
+        <button onClick={() => setActiveTab(2)}>Aktif Tabı Değiştir</button>
+        <Tab
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          onChange={(tabIndex) => setActiveTab(tabIndex)}
+        >
+          <Tab.Panel title="Profil">1.Tab</Tab.Panel>
+          <Tab.Panel title="Hakkımda">2.Tab</Tab.Panel>
+          <Tab.Panel title="Ayarlar">3.Tab</Tab.Panel>
+        </Tab>
+        {activeTab === 2 && <div>Burasıda Ekstra Bir Alan</div> }
+      </div>
+
+      <div style={{ padding: 20 }}>
+        <Button>Button örneği</Button>
+        <Button variant="succes">Button örneği</Button>
+        <Button variant="danger">Button örneği</Button>
+        <Button variant="warning">Button örneği</Button>
+      </div>
+    </>
   );
 }
 
