c9a5dd66289719f5b282da32c36f888675980653
Utku Cengiz <cekicengiz01@gmail.com>
2022-12-17 21:41:06 +0300

    Yaşam Döngüsü (Lifecycle)

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import { useEffect, useState } from "react";
-import Button from "./Components/Button";
-import Tab from "./Components/Tab";
-import "./tailwind.css";
+import Test from "./Test";
+import { useState } from "react";
 
 function App() {
-  const [activeTab, setActiveTab] = useState(1);
-
+  const [show, setShow] = useState(false);
   return (
     <>
-      <div style={{ padding: 20 }}>
-        <button onClick={() => setActiveTab(2)}>Aktif Tabı Değiştir</button>
-        <Tab
-          activeTab={activeTab}
-          setActiveTab={setActiveTab}
-          onChange={(tabIndex) => setActiveTab(tabIndex)}
-        >
-          <Tab.Panel title="Profil">1.Tab</Tab.Panel>
-          <Tab.Panel title="Hakkımda">2.Tab</Tab.Panel>
-          <Tab.Panel title="Ayarlar">3.Tab</Tab.Panel>
-        </Tab>
-        {activeTab === 2 && <div>Burasıda Ekstra Bir Alan</div> }
-      </div>
-
-      <div style={{ padding: 20 }}>
-        <Button>Button örneği</Button>
-        <Button variant="succes">Button örneği</Button>
-        <Button variant="danger">Button örneği</Button>
-        <Button variant="warning">Button örneği</Button>
-      </div>
+      <button onClick={() => setShow((show) => !show)}>
+        {show ? "Gizle" : "Göster"}
+      </button>
+      {show && <Test />}
     </>
   );
 }
